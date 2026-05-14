88e1435ac9bee913a7cb20c64dbde0e691094072
Christopher <gordonchristopher453@gmail.com>
2026-05-14 09:51:40 +0300

    Clear PWA cache force refresh

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,30 +1,18 @@
-const CACHE_NAME = 'dark-horse-v1'
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-]
+const CACHE_NAME = 'dark-horse-v2'
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  )
   self.skipWaiting()
 })
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   )
   self.clients.claim()
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-    })
-  )
+  event.respondWith(fetch(event.request))
 })
